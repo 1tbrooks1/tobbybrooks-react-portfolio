cb2758bdbe9b40523294553a6b533bdabbf20fb7
tbrooks <tbrooks321@yahoo.com>
2022-04-16 16:36:28 -0400

    minor changes

--- src/App.js
+++ src/App.js
@@ -6,13 +6,59 @@ import Projects from "./components/Projects"
 import Resume from "./components/Resume"
 
 function App() {
+  const [projects] = useState([
+    {
+      title: 'test1',
+      // image: '',
+      appLink: '',
+      gitHubLink: '',
+    },
+    {
+      title: 'test2',
+      // image: '',
+      appLink: '',
+      gitHubLink: '',
+    },
+    {
+      title: 'test3',
+      // image: '',
+      appLink: '',
+      gitHubLink: '',
+    },
+    {
+      title: 'test4',
+      // image: '',
+      appLink: '',
+      gitHubLink: '',
+    },
+    {
+      title: 'test5',
+      // image: '',
+      appLink: '',
+      gitHubLink: '',
+    },
+    {
+      title: 'test6',
+      // image: '',
+      appLink: '',
+      gitHubLink: '',
+    },
+  ]);
+
+  const [currentProject, setCurrentProject] = useState(projects[0]);
+
   return (
     <div>
-      <Nav></Nav>
+      <Nav>
+      </Nav>
       <main>
       <About></About>
       <Contact></Contact>
-      <Projects></Projects>
+      {/* <Projects
+       projects={projects}
+       setCurrentProject={setCurrentProject}
+       currentProject={currentProject}
+      ></Projects> */}
       {/* <Resume></Resume> */}
       </main>
     </div>
--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -1,11 +1,27 @@
 import React from "react";
+import { capitalizeFirstLetter } from '../utils/helper';
+import { Card } from 'react-bootstrap';
+import { Button } from 'react-bootstrap'
 
 
-function Projects() {
+function Projects(props) {
+  const {
+    projects = [],
+    setCurrentProject,
+    currentProject,
+  } = props;
+  
   return (
-   <section>
-     hello
-   </section>
+    <section>
+{projects.map((project) => (
+  <li
+  key={project.title}
+  />
+))}
+<span>
+    {project.title}
+  </span>
+  </section>
   );
 }
 export default Projects;
