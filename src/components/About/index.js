d5acf8f3c20e52344592de25cf3362755ce8b803
tbrooks <tbrooks321@yahoo.com>
2022-04-17 15:51:48 -0400

    worked on project cards conditonally rendering and about me section

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,16 +1,17 @@
 import React from 'react'
-import profPic from "../../assets/images/profPic.jpg"
+import profPic from "../../assets/images/profPic.png"
 
 function About() {
   return (
-    <section className="d-flex justify-content-center align-items-center flex-wrap">
-      <h1 className="pt-5">About Me</h1>
+    <section >
+      <h1 className="d-flex justify-content-center align-items-center flex-wrap">About Me</h1>
       <div className="">
-      <img className="mx-auto d-block rounded pt-2" style={{ width: "25%" }} alt="profile" src={profPic}></img>
-      <p className="text-center pt-4">Hello, my name is Tobby Brooks and welcome to my portfolio.
-      Currently, I'm an aspiring Full Stack Software Developer attending a Full Stack Software
-      Developer bootcamp and George Washington University with a future completion date of early/mid May 2022.
-      After completion of my course, I will be looking for full time employment as a software developer.</p>
+      <img className="mx-auto d-block rounded" style={{ width: "16%" }} alt="profile" src={profPic}></img>
+      <p className='text-center'>Hello, my name is Tobby Brooks and welcome to my portfolio.
+      Currently, I'm an aspiring Full Stack Software Developer attending a Full Stack software<br></br>
+      Developer bootcamp at George Washington University with a future completion date of early/mid May 2022.
+      After completion of my course, I will be looking<br></br> 
+      for full time employment as a software developer.</p>
       </div>
     </section>
   )
--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -1,65 +1,74 @@
-import React from "react";
+import React, { useState } from "react";
 import Card from "react-bootstrap/Card";
-import Button from "react-bootstrap/Button";
 
 function Projects() {
-  // const [projects] = useState([
-  //   {
-  //     title: "Sole Intentions",
-  //    image: "",
-  //     description: "",
-  //     link: "https://soleintentions.herokuapp.com/",
-  //     git: "https://github.com/1tbrooks1/sole_intentions",
-  //   },
-  //   {
-  //     title: "Newsify",
-  //    image: "",
-  //     description: "",
-  //     link: "https://yoowook1207.github.io/Newsify/",
-  //     git: "https://github.com/1tbrooks1/Newsify",
-  //   },
-  //   {
-  //     title: "Budget Tracker",
-  //    image: "",
-  //     description: "",
-  //     link: "https://financialmonitor.herokuapp.com/",
-  //     git: "https://github.com/1tbrooks1/tb-budget-tracker",
-  //   },
-  //   {
-  //     title: "Note Taker",
-  //    image: "",
-  //     description: "",
-  //     link: "https://tb-note-taker-app.herokuapp.com/",
-  //     git: "https://github.com/1tbrooks1/tb-note-taker",
-  //   },
-  //   {
-  //     title: "Weather Dashboard",
-  //    image: "",
-  //     description: "",
-  //     link: "https://1tbrooks1.github.io/weather-dashboard/",
-  //     git: "https://github.com/1tbrooks1/weather-dashboard",
-  //   },
-  //   {
-  //     title: "Work Day",
-  //    image: "",
-  //     description: "",
-  //     link: "https://1tbrooks1.github.io/work-day/",
-  //     git: "https://github.com/1tbrooks1/work-day",
-  //   },
-  // ])
+   const [portfolio] = useState([
+    {
+       title: "Sole Intentions",
+       name: "sole",
+       description: "test",
+       link: "https://soleintentions.herokuapp.com/",
+       git: "https://github.com/1tbrooks1/sole_intentions",
+     },
+     {
+       title: "Newsify",
+       name: "newsify",
+       description: "test",
+       link: "https://yoowook1207.github.io/Newsify/",
+       git: "https://github.com/1tbrooks1/Newsify",
+     },
+     {
+       title: "Budget Tracker",
+       name: "budget",
+       description: "test",
+       link: "https://financialmonitor.herokuapp.com/",
+       git: "https://github.com/1tbrooks1/tb-budget-tracker",
+     },
+     {
+       title: "Note Taker",
+       name: "note",
+       description: "test",
+       link: "https://tb-note-taker-app.herokuapp.com/",
+       git: "https://github.com/1tbrooks1/tb-note-taker",
+     },
+     {
+       title: "Weather Dashboard",
+       name: "weather",
+       description: "test",
+       link: "https://1tbrooks1.github.io/weather-dashboard/",
+       git: "https://github.com/1tbrooks1/weather-dashboard",
+     },
+     {
+       title: "Work Day",
+       name: "work-day",
+       description: "test",
+       link: "https://1tbrooks1.github.io/work-day/",
+       git: "https://github.com/1tbrooks1/work-day",
+     },
+   ])
 
   return (
-    <Card style={{ width: "18rem" }}>
-      <Card.Img variant="top" src="holder.js/100px180" />
-      <Card.Body>
-        <Card.Title>Card Title</Card.Title>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </Card.Text>
-        <Button variant="primary">Go somewhere</Button>
-      </Card.Body>
-    </Card>
-  );
+    <div className="d-flex justify-content-around flex-wrap">
+      {portfolio.map((project) => (
+        <div>
+          <Card className="m-1 card-content" style={{ width: '18rem' }}>
+            <Card.Img
+              variant="top"
+              src={require(`../../assets/images/${project.name}.png`)}
+            />
+            <Card.Body>
+              <Card.Title>{project.title}</Card.Title>
+              <Card.Text>{project.description}</Card.Text>
+            </Card.Body>
+            <Card.Body>
+              <Card.Link href={project.git}>GitHub</Card.Link><br></br>
+                <Card.Link href={project.link}>Deployed App</Card.Link>
+            </Card.Body>
+          </Card>
+        </div>
+      ))}
+    </div>
+  )
 }
+
 export default Projects;
