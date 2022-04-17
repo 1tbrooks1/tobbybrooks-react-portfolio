aa57bcbab6825cfbd1cef1a304a02f14af52dda0
tbrooks <tbrooks321@yahoo.com>
2022-04-17 16:27:05 -0400

    added links to socials in footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -5,29 +5,37 @@ import {
   faLinkedin,
   faStackOverflow,
 } from "@fortawesome/free-brands-svg-icons";
-import "./style.css"
+import "./style.css";
 
 function Footer() {
   return (
-  
     <div class="social-container">
-    <h3 className="text-center">Social Follow</h3>
-    <div className="d-flex flex-row justify-content-center">
-    <a href="https://www.youtube.com/c/jamesqquick"
-        className="youtube social">
-        <FontAwesomeIcon icon={faGithub} size="2x" />
-      </a>
-      <a href="https://www.facebook.com/learnbuildteach/"
-        className="facebook social">
-        <FontAwesomeIcon icon={faLinkedin} size="2x" />
-      </a>
-      <a href="https://www.twitter.com/jamesqquick" className="twitter social">
-        <FontAwesomeIcon icon={faStackOverflow} size="2x" />
-      </a>
+      <h3 className="text-center">Social Follow</h3>
+      <div className="d-flex flex-row justify-content-center">
+        <a
+          href="https://github.com/1tbrooks1"
+          target="_blank"
+          className="github social"
+        >
+          <FontAwesomeIcon icon={faGithub} size="2x" />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/tobby-brooks/"
+          target="_blank"
+          className="linked social"
+        >
+          <FontAwesomeIcon icon={faLinkedin} size="2x" />
+        </a>
+        <a
+          href="https://stackoverflow.com/users/17193167/tbrooks"
+          target="_blank"
+          className="stack social"
+        >
+          <FontAwesomeIcon icon={faStackOverflow} size="2x" />
+        </a>
       </div>
-</div>
-
-  )
+    </div>
+  );
 }
 
 export default Footer;
