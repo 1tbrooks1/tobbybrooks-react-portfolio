681be48d5db8a68a20842ac71c397a8f479ba6a5
tbrooks <tbrooks321@yahoo.com>
2022-04-17 21:05:33 -0400

    updated bio in about

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -9,10 +9,14 @@ function About() {
       <div>
       <img className="image d-block"  alt="profile" src={profPic}></img>
       <p>Hello, my name is Tobby Brooks and welcome to my portfolio.
-      Currently, I'm an aspiring Full Stack Software Developer attending a Full Stack software<br></br>
+      Currently, I'm an aspiring Full Stack Software Developer attending a Full Stack software<br />
       Developer bootcamp at George Washington University with a future completion date of early/mid May 2022.
-      After completion of my course, I will be looking<br></br> 
-      for full time employment as a software developer.</p>
+      After completion of my course, I will be looking
+      for full time employment as a software developer. <br /> 
+      I'm very hardworking, ambitious,
+      love problem solving, and work well with others. My portfolio will give you a view 
+      into my journey through programming where you'll be able to see some of my projects 
+      I've worked on, a list of my skills, and my resume.</p>
       </div>
     </section>
   )
